3ec8ed19e382322065bee8bf9a53faa49fb8a1d8
Madruga <madrugayh@keemail.me>
2026-08-31 03:35:00 -0300

    Implement teclado_func for app search functionality
    Added teclado_func to handle user input for searching apps.

--- lib/api.sh
+++ lib/api.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-#Function Sound Up
 volumeup() {
   sleep 0.2
   clear 
@@ -35,7 +34,6 @@ fi
 done
 }
 
-#Function Sound Down
 volumedown() {
   sleep 0.2
   clear 
@@ -120,6 +118,89 @@ mutartv_func() {
    sleep 3
    exit 1
   fi
+}
+
+teclado_func() {
+  while true; do
+sleep 0.5
+clear
+sleep 0.5
+echo -e "${AMARELO}Enter the text you want to search or Press 0 to back to menu ${RESET}"
+sleep 0.5
+echo "                                                               "
+sleep 0.5
+read -rp "$(echo -e "${AMARELO}>  ${VERDE}")" texto_user
+sleep 0.5
+echo "                                                               "
+sleep 1
+echo -e "${AMARELO}In which app do you want to search for this? ${RESET}"
+sleep 0.3
+echo -e "${AMARELO}1) Netflix ${RESET}"
+sleep 0.3
+echo -e "${AMARELO}2) Spotify ${RESET}"
+sleep 0.3
+echo -e "${AMARELO}3) Youtube ${RESET}"
+sleep 0.3
+echo -e "${AMARELO}4) Browse of system ${RESET}"
+sleep 0.3
+echo "                                                               "
+sleep 0.3
+read -rp "$(echo -e "${AMARELO}>  ${VERDE}")" app_choice  
+
+if [[ "$texto_user" == "0" ]]; then
+    sleep 0.3
+    clear
+    sleep 0.5
+    main_menu
+fi
+
+if [[ "$app_choice" == "1" ]]; then
+    curl -d '' "http://${Ip}:8060/launch/12?contentId=search" > /dev/null 2&1
+    sleep 7
+elif [[ "$app_choice" == "2" ]]; then
+    curl -d '' "http://${Ip}:8060/launch/19977?contentId=search" > /dev/null 2&1
+    sleep 7
+elif [[ "$app_choice" == "3" ]]; then
+    curl -d '' "http://${Ip}:8060/launch/837?contentId=search" > /dev/null 2&1
+sleep 7
+elif [[ "$app_choice" == "4" ]]; then
+    curl -d '' "http://${Ip}:8060/search/browse?keyword=" > /dev/null 2&1
+    sleep 5
+else
+    sleep 0.5
+    echo "                                                        "
+    echo -e "${VERMELHO}Error: Invalid option!${RESET}"
+    sleep 0.5
+    continue
+fi
+
+for (( i=0; i<${#texto_user}; i++ )); do
+    CARACTERE="${texto_user:$i:1}"
+    
+    CHAR_CODIFICADO=$(echo -n "$CARACTERE" | xxd -plain | tr -d '\n' | sed 's/\(..\)/%\1/g')
+    
+    if curl --connect-timeout 3 --max-time 4 -d '' "http://${Ip}:8060/keypress/Lit_${CHAR_CODIFICADO}" > /dev/null 2>&1; then
+
+    sleep 0.2
+    if curl --connect-timeout 3 --max-time 4 -d '' "http://${Ip}:8060/keypress/Enter" > /dev/null 2>&1; then
+    echo -e "${VERDE}Text sent successfully!${RESET}"
+    else
+        sleep 0.5
+        clear
+        sleep 1.2
+        echo -e "${VERMELHO}Error: communication with device failed ${RESET}"
+        sleep 0.2
+        echo "                                                            "
+        sleep 0.5
+        echo -e "${VERMELHO}Exiting... ${RESET}"
+        sleep 3
+        exit 1
+    fi
+    fi
+done
+done
+}
+
 }
 
 
