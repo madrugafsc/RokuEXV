773e3f45d30be22e3911e10001831961d7841007
Madruga <madrugayh@keemail.me>
2026-08-31 16:01:49 -0300

    Update rokuexv.sh

--- rokuexv.sh
+++ rokuexv.sh
@@ -23,35 +23,6 @@ done
 validar_arg
 
 main() {
-    sleep 0.5
-    clear
-    sleep 0.5
-    echo -e "${VERMELHO}⠀⠀⠀⠀⠀⠀⠀⢠⣀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⡄⠀        (                             )          ${RESET}"
-    sleep 0.2
-    echo -e "${VERMELHO}⠀⠀⠀    ⠀⠀⠉⠻⣦⣄⠀⠀⠀⠀⠀⠀⣠⣴⠟⠉⠀⠀⠀⠀⠀⠀⠀⠀ )\\ )         )             ( /(          ${RESET}"
-    sleep 0.2
-    echo -e "${VERMELHO}⠀⠀⠀⠀⠀ ⠀⠀⠀⠀⠀⠉⣠⣴⣶⣶⣦⣄⠉⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀  ((()/(      ( /(    (   (    )\()) (   (   ${RESET}"
-    sleep 0.2
-    echo -e "${VERMELHO}⠀⠀⠀⣀⣀⣀⣀⣀⣀⣀⣀⣀⣉⣉⣉⣉⣉⣉⣀⣀⣀⣀⣀⣀⣀⣀⣀⠀⠀⠀ /(_)) (   )\\())  ))\\  )\\  ((_)\\  )\\  )\\  ${RESET}"
-    sleep 0.2
-    echo -e "${VERMELHO}⠀⠀⢸⣿⠟⠛⣛⣛⣛⣛⣛⣛⣛⣛⣛⣛⣛⣛⠛⠛⠛⠛⢿⡿⠛⠿⣿⡇⠀⠀ (_))   )\\ ((_)\\  /((_)((_) __((_)((_)((_) ${RESET}"
-    sleep 0.2
-    echo -e "${VERMELHO}  ⢸⡏⢠⣾⣿⣿⣿⣿⣿⠿⠛⠋⠉⠁⠀⠀⠀⠀⠀⠀⢸⣇⠀⠀⣽⡇⠀⠀| _ \\ ((_)| |(_)(_))(| __|\\ \\/ /\\ \\ / /  ${RESET}"
-    sleep 0.2
-    echo -e "${VERMELHO}⠀⠀⢸⡇⢸⣿⣿⣿⠟⠋⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⣿⣿⣿⣿⡇⠀⠀|   //_ \\| / / | ||| | _|  >  <  \\ V /   ${RESET}"
-    sleep 0.2
-    echo -e "${VERMELHO}⠀⠀⢸⡇⢸⣿⡿⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⣿⣿⣿⣿⡇⠀⠀|_|_\\\\___/|_\\_\\ \\_,_| |___|/_/\\_\\  \\_/    ${RESET}  ${AMARELO}V 1.0${RESET}"
-    sleep 0.2
-    echo -e "${VERMELHO}⠀⠀⢸⡇⢸⣿⡿⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⣿⣿⣿⣿⡇ ${RESET}  ${AMARELO}[${VERDE}Coded by Madruga${RESET}${RESET}${AMARELO}]${RESET}"    
-    sleep 0.2
-    echo -e "${LARANJA}  ⢸⡇⠸⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⡷⠶⠶⣾⡇⠀⠀${RESET} ${AMARELO}[${VERDE}Github: madrugafsc${RESET}${AMARELO}]${RESET}"
-    sleep 0.2
-    echo -e "${LARANJA}⠀ ⢸⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⡷⠶⠶⢾⡇⠀⠀${RESET} ${AMARELO}[${VERDE}Discord: little_madruga${RESET}${AMARELO}]${RESET}"
-    sleep 0.2
-    echo -e "${LARANJA} ⠀⢸⣿⣄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣼⣷⣶⣶⣿⡇⠀⠀${RESET} ${AMARELO}[${VERDE}Email: madrugayh@keemail.me${RESET}${AMARELO}]${RESET}"
-    sleep 0.2
-    echo -e "${LARANJA}  ⠘⠛⠛⠛⠛⠛⠛⠛⠛⠛⠛⠛⠛⠛⠛⠛⠛⠛⠛⠛⠛⠛⠛⠛⠛⠃${RESET}"
-    echo ""
     main_menu                        
 }
 
